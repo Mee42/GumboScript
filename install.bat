81bce3b570062b84140581511f2aa5ac9273e33f
Carson Graham <carson42g@gmail.com>
2019-03-29 13:30:33 -0700

    started install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-This file is currently incomplete
-Please do not run
+mkdir "%APPDATA%/Gumboscript"
+curl -o "%APPDATA%\Gumboscript\gumbo.jar" "http://72.66.54.109:8000/quick/GumboScript/build/libs/GumboScript-1.0-SNAPSHOT-all.jar"
