91a8559d87c571f473600f05f157e160c335c914
Carson Graham <carson42g@gmail.com>
2019-03-29 17:26:16 -0700

    added the windows stuff

--- install.bat
+++ install.bat
@@ -1,2 +1,8 @@
-mkdir "%APPDATA%/Gumboscript"
+@echo off
+
+mkdir "%APPDATA%\Gumboscript"
+mkdir "%APPDATA%\Gumboscript\backups"
 curl -o "%APPDATA%\Gumboscript\gumbo.jar" "http://72.66.54.109:8000/quick/GumboScript/build/libs/GumboScript-1.0-SNAPSHOT-all.jar"
+curl -o "%APPDATA%\Gumboscript\gumbo_path.bat" "http://72.66.54.109:8000/quick/GumboScript/windows/gumbo_path.bat"
+curl -o "%APPDATA%\Gumboscript\gumbo.bat" "http://72.66.54.109:8000/quick/GumboScript/windows/gumbo.bat"
+%APPDATA%\Gumboscript\gumbo_path.bat
