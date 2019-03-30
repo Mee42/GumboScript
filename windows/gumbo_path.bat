7d5a275f9c586d122c835de02f6aaaac7e163b7c
Carson Graham <carson42g@gmail.com>
2019-03-30 07:24:17 -0700

    added backup files that have setx

--- windows/gumbo_path.bat
+++ windows/gumbo_path.bat
@@ -9,5 +9,5 @@ echo %E%
 if %E% NEQ 0 echo Running!
 
 if %E% NEQ 0 echo %PATH%
-if %E% NEQ 0 echo %PATH% > %APPDATA%\Gumboscript\backups\%RANDOM%.backup.path
+if %E% NEQ 0 echo setx PATH "%PATH%" > %APPDATA%\Gumboscript\backups\%RANDOM%backup.bat
 if %E% NEQ 0 setx PATH "%PATH%;%APPDATA%\Gumboscript"
